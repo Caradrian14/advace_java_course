9843e246dd11363c686d463612acf15ac247b5d2
Adria <adria.jorda.m@gmail.com>
2025-09-11 09:49:49 +0200

    [add] lectura 3

--- course_java_1/src/main/java/org/example/prueba_lectura2/answer1.java
+++ course_java_1/src/main/java/org/example/prueba_lectura2/answer1.java
@@ -1,9 +1,6 @@
 package org.example.prueba_lectura2;
 import java.util.stream.IntStream;
 
-
-//public class respuestas_1 { }
-
 public class answer1 {
     public static void main(String[] args) {
         double average = IntStream.range(0, 5)
