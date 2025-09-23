ce6b517c26fd8a63aff1b3234b3708d088a9ef76
Adria <adria.jorda.m@gmail.com>
2025-09-23 17:21:52 +0200

    [add] lectura13

---
+++ course_java_1/src/main/java/org/example/lecture13/TypesOfModules/automatic/text-utils/src/text/utils/Utils.java
@@ -0,0 +1,8 @@
+package text.utils;
+
+public class Utils{
+  public static String upper(String in){
+    return in.toUpperCase();
+
+  }
+}
---
+++ course_java_1/src/main/java/org/example/lecture13/TypesOfModules/automatic/text.app/src/sample.typing/sample/typing/Main.java
@@ -0,0 +1,10 @@
+package sample.typing;
+
+import text.utils.Utils;
+
+public class Main{
+  public static void main(String[] args){
+    	String uppercase = Utils.upper("this is lower");
+	    System.out.println(uppercase);
+  }
+}
